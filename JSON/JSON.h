9d1de770db581ae3246e2d7ddf71cd271fd070fc
David Owens II <owensd@kiadsoftware.com>
2014-06-20 00:22:53 -0700

    Initial Commit

---
+++ JSON/JSON.h
@@ -0,0 +1,19 @@
+//
+//  JSON.h
+//  JSON
+//
+//  Created by David Owens on 6/20/14.
+//  Copyright (c) 2014 Kiad Software. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for JSON.
+FOUNDATION_EXPORT double JSONVersionNumber;
+
+//! Project version string for JSON.
+FOUNDATION_EXPORT const unsigned char JSONVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JSON/PublicHeader.h>
+
+
