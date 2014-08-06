a6826039dc8f8899f20dd6c6f018a7b7632f7d7a
David Owens II <owensd@kiadsoftware.com>
2014-08-06 10:47:54 -0700

    The @autoclosure() is actually not safe, back to FailableWrapper<T>

--- JSON/Failable.swift
+++ JSON/Failable.swift
@@ -37,44 +37,50 @@ public enum Failable {
 /// A `FailableOf<T>` should be returned from functions that need to return success or failure information and some
 /// corresponding data back upon a successful function call.
 public enum FailableOf<T> {
-    case Success(@autoclosure() -> T)
+    case Success(FailableValueWrapper<T>)
     case Failure(Error)
-
+    
     public init(_ value: T) {
-        self = .Success(value)
+        self = .Success(FailableValueWrapper(value))
     }
-
+    
     public init(_ error: Error) {
         self = .Failure(error)
     }
-
+    
     public var failed: Bool {
         switch self {
         case .Failure(let error):
             return true
-
+            
         default:
             return false
-        }
+            }
     }
-
+    
     public var error: Error? {
         switch self {
         case .Failure(let error):
             return error
-
+            
         default:
             return nil
-        }
+            }
     }
-
+    
     public var value: T? {
         switch self {
-        case .Success(let value):
-            return value()
-
+        case .Success(let wrapper):
+            return wrapper.value
+            
         default:
             return nil
-        }
+            }
     }
+}
+
+/// This is a workaround-wrapper class for a bug in the Swift compiler. DO NOT USE THIS CLASS!!
+public class FailableValueWrapper<T> {
+    public let value: T
+    public init(_ value: T) { self.value = value }
 }
