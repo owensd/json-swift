e46d984a54f31543dd1cc6eddb04c3061643a804
David Owens II <david@owensd.io>
2017-05-21 00:44:28 -0700

    Goodness... parser_tests.sh should actually be fixed now.

--- Scripts/parser_tests.sh
+++ Scripts/parser_tests.sh
@@ -18,10 +18,11 @@ done
 
 for f in $TestCollateral/JSONTestSuite/test_parsing/n_*.json; do
     $TestHarness $f
-    if [ $? -eq 0 ]; then
+    ReturnValue=$?
+    if [ $ReturnValue -eq 0 ]; then
         SuccessCount=$(expr $SuccessCount + 1)
     else
-        if [ $? -eq 139 ]; then
+        if [ $ReturnValue -eq 139 ]; then
             echo "!!!!! Crash while testing: $f"
         fi
         FailureCount=$(expr $FailureCount + 1)
